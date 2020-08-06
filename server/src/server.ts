b2935ec208fa58f57e5ff1cdae3bd9d39124ef80
Joao Zandona <joaoricardozandona@hotmail.com>
2020-08-06 17:35:08 -0300

    Aula 2 mais ou menos uma hora e quinze

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,20 @@
-console.log('Hello World');
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+//GET: Buscar ou listar uma informação
+//POST: Criar alguma nova informação
+//PUT: Atualiar uma informação existente
+//DELETE: Deletar uma informação existente
+
+//Corpo (Request Body): Dados para a criação ou atualização de um registro
+//Route Params: Identificar qual recurso eu quero atualizar ou deletar
+//Query Params: Paginação, filtros, ordenação
+
+    app.get('/', (request, response) => {
+        return response.json({message: 'Hello World'});
+    });
+app.listen(3333);
+
